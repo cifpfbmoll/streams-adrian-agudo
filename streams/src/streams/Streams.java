0b40551072dc6be36e62e4e5d1de42b6206c7897
Adrián Agudo Bruno <56310444+aagudobruno@users.noreply.github.com>
2020-03-29 18:14:44 +0200

    Add files via upload

--- streams/src/streams/Streams.java
+++ streams/src/streams/Streams.java
@@ -5,8 +5,12 @@
  */
 package streams;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -15,81 +19,159 @@ import java.util.Scanner;
  * @author ADRI
  */
 public class Streams {
-
+    static int contadorAlmo=0;
+    static boolean escribirDoc=false;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args){
             int opcion=0; 
-            int car=0;
-            int contadorAlmo=0;
-            int contadorCar=0;
+            int caracter=0;
+            String ruta="";
+            String linea="";
+            int i=0;
             while (opcion !=4){
-                System.out.println("\nMENU"+"\n1.-Lectura y escritura del fichero de cartelera byte a byte (byte Streams).\n" +
+                System.out.print("\n\nMENU"+"\n1.-Lectura y escritura del fichero de cartelera byte a byte (byte Streams).\n" +
                 "2.-Lectura y escritura de fichero de cartelera carácter a carácter (character Streams).\n" +
                 "3.-Lectura y escritura de fichero línea a línea con buffers (character Streams).\n" +
-                "4.- Salir");
+                "4.- Salir" +
+                "\n\nSeleccionar opción: ");
                 Scanner lector=new Scanner (System.in);
                 opcion=Integer.parseInt(lector.nextLine());
-                switch (opcion){
+                if (opcion!=4){
+                    System.out.print("\nIntroducir ruta del fichero: ");
+                    ruta=lector.nextLine();
+                }
+                switch (opcion){                        
                     case 1:
                         try{
-                            FileInputStream fin=new FileInputStream("C:\\Users\\ADRI\\Desktop\\Practica7.txt");
-                             System.out.println("====================================");
-                             System.out.println("===== CARTELERA DE CINE FBMOLL =====");
-                             System.out.println("====================================");
-                             System.out.print("----");
-                            do{
-                              car=fin.read();
-                              switch ((char)car){
-                                  case '#':
-                                       switch (contadorAlmo){
-                                           case 0:
-                                               System.out.print("----\nAño:");
-                                               break;
-                                           case 1: 
-                                               System.out.print("\nDirector:");
-                                               break;
-                                           case 2:
-                                               System.out.print("\nDuración:");
-                                               break;
-                                           case 3: 
-                                               System.out.print(" minutos\nSypnosis:");
-                                               break;
-                                           case 4:
-                                               System.out.print("\nReparto:");
-                                               break;
-                                           case 5:
-                                               System.out.print("\nSesión:");
-                                               break;
-                                       }
-                                       contadorAlmo++;
-                                       break;
-                                  case '{':
-                                      System.out.print("\n----");
-                                      contadorAlmo=0;
-                                      break;
-                                  default:   
-                                     if ((int)car!=-1){ 
-                                        if (contadorCar%62!=0){ 
-                                            System.out.print((char)car);
-                                        }else{
-                                             System.out.print("\n"+(char)car);
-                                        }
-                                        contadorCar++;
-                                     }
-                                     break;
-                              }
+                            FileInputStream fin=new FileInputStream(ruta);
+                            FileOutputStream fout=new FileOutputStream(ruta);
+                            imprimirCabecera();
+                            caracter = fin.read();
+                            while (caracter!=-1){
+                                caracterAcaracter(caracter);
+                                caracter = fin.read();
                             }
-                            while (car!=-1);         
+                            contadorAlmo=0;
+                            caracter=0;
+                            fin.close();
+                            linea=escribir();
+                            if (escribirDoc){
+                                for (i=0;i<linea.length();i++){
+                                    fout.write((int)linea.charAt(i));
+                                }
+                            }
+                            escribirDoc=false;
                         }catch(FileNotFoundException ex){
                             System.err.println("Fichero no encontrado");
                         }catch(IOException ex2){
-                            System.err.println(ex2.getMessage());
-                        }  
-                    break;
+                            System.err.println(ex2.getMessage()); 
+                        }
+                        break;
+                    case 2:
+                        try{
+                            File entrada=new File(ruta); 
+                            FileReader lectorFich=new FileReader(entrada);
+                            imprimirCabecera();
+                            caracter = lectorFich.read();
+                            while (caracter!=-1){
+                                caracterAcaracter(caracter);
+                                caracter = lectorFich.read();
+                            }
+                            contadorAlmo=0;
+                            caracter=0;
+                            lectorFich.close();
+                        }catch(FileNotFoundException ex){
+                            System.err.println("Fichero no encontrado");
+                        }catch(IOException ex2){
+                            System.err.println(ex2.getMessage()); 
+                        }
+                    case 3: 
+                        try{
+                            File entrada=new File(ruta); 
+                            BufferedReader lectorMasMejor=new BufferedReader(new FileReader(entrada));
+                            imprimirCabecera();
+                            linea=lectorMasMejor.readLine();
+                            while (linea != null){
+                                for (i=0;i<linea.length();i++){
+                                    caracterAcaracter((int)linea.charAt(i));
+                                }
+                                linea=lectorMasMejor.readLine();
+                            }
+                            contadorAlmo=0;
+                            caracter=0;
+                            linea="";
+                            lectorMasMejor.close();
+                        }catch(FileNotFoundException ex){
+                            System.err.println("Fichero no encontrado");
+                        }catch(IOException ex2){
+                            System.err.println(ex2.getMessage()); 
+                        }
                 }
             }
+    }        
+    public static void caracterAcaracter(int car) throws IOException{
+            switch ((char) car) {
+                case '#':
+                    switch (contadorAlmo) {
+                        case 0:
+                            System.out.print("----\nAño: ");
+                            break;
+                        case 1:
+                            System.out.print("\nDirector: ");
+                            break;
+                        case 2:
+                            System.out.print("\nDuración: ");
+                            break;
+                        case 3:
+                            System.out.print(" minutos\nSypnosis: ");
+                            break;
+                        case 4:
+                            System.out.print("\nReparto: ");
+                            break;
+                        case 5:
+                            System.out.print("\nSesión: ");
+                            break;
+                    }
+                    contadorAlmo++;
+                    break;
+                case '{':
+                    System.out.print("\n----");
+                    contadorAlmo = 0;
+                    break;
+                default:
+                    if ((int) car != -1) {
+                        System.out.print((char) car);
+                    break;    
+                    }
+            }
+        }   
+
+    public static void imprimirCabecera() {
+        System.out.println("\n====================================");
+        System.out.println("===== CARTELERA DE CINE FBMOLL =====");
+        System.out.println("====================================");
+        System.out.print("----");
     }
     
+    public static String escribir() {
+        Scanner lector=new Scanner(System.in);
+        String opcion="";
+        String texto=""; 
+        System.out.print("\n\nDesea escribir en el documento?(S/N): ");
+        opcion=lector.nextLine();
+        if (opcion.equals("S")||opcion.equals("s")){
+            System.out.print("\n\nIntroducir texto a escribir: ");
+            texto=lector.nextLine();
+            escribirDoc=true;
+        }
+        return texto;
+    }
 }
+  
+            
+            
+    
+    
+
